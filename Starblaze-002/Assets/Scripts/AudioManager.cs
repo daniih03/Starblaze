ff8240e71455bd234e1880702bb8a352bf0dfeb9
josemamm <josemamm._@hotmail.com>
2023-05-03 15:44:16 +0200

    aasda

--- Starblaze-002/Assets/Scripts/AudioManager.cs
+++ Starblaze-002/Assets/Scripts/AudioManager.cs
@@ -30,19 +30,7 @@ private void Awake()
     void Update()
     {
 
-        if(PlayerController.instance.anim.GetBool("Dead"))
-        {
-            Fadetime = Fadetime - Time.deltaTime;
-            if(Fadetime <= 0)
-            {
-                bgm.volume = Mathf.MoveTowards(bgm.volume, 0.01f, 0.05f *Time.deltaTime);
-            } 
-            
-        } else
-        {
-            Fadetime = fadetime;
-            bgm.volume = Mathf.MoveTowards(bgm.volume, 0.05f, 0.1f *Time.deltaTime);
-        } 
+        
     }
 
     public void PlaySFX(int soundToPlay)
